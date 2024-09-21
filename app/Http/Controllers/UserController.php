562a4b55d6c045adb702fbcd12512ee804940ec0
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-09-21 21:10:54 +0600

    User-Registration-and-Backend-Testing

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function UserRegistration(Request $request) {
+        try {
+            User::create([
+                'firstName' => $request->input('firstName'),
+                'lastName' => $request->input('lastName'),
+                'email' => $request->input('email'),
+                'mobile' => $request->input('mobile'),
+                'password' => $request->input('password'),
+            ]);
+            return response()->json([
+                'status' => 'Success',
+                'message' => 'User Registration Successfully.'
+            ], 200);
+        }
+        catch (Exception $e) {
+            return response()->json([
+                'status' => 'Failed',
+//                'message' => $e->getMessage()
+                'message' => 'User Registration Failed.'
+            ], 200);
+        }
+    }
+}
